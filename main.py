4bc05f5c00a1c8d0a5396604d848e1f2ba2e7e08
Ricsius <hajnal.ricsi@gmail.com>
2024-11-14 20:40:14 +0100

    Add links, specify language, limit news, control topic

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ import os
 from send_email import send
 
 api_key = os.getenv("News_API_Key")
-url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey={api_key}"
+topic = "tesla"
+language = "en"
+url = "https://newsapi.org/v2/everything?"\
+    f"q={topic}&"\
+    "sortBy=publishedAt&"\
+    f"apiKey={api_key}&"\
+    f"language={language}"
 
 request = requests.get(url)
 content = request.json() 
 articles = ""
 
-for article in content["articles"]:
+for article in content["articles"][:20]:
     title = article["title"]
     description = article["description"]
+    link = article["url"]
 
     if title is not None and description is not None:
-        articles += title + "\n" + description + "\n\n"
+        articles += title + "\n" + description + "\n" + link + "\n\n"
 
 message = """\
-Subject: Newsletter
+Subject: Today's news
 
 PythonHow newsletter:\n\n
 """
